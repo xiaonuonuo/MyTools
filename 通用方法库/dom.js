37135b010e655cf3274991da82bd48845e0b882e
xiaonuonuo <272154379@qq.com>
2018-08-04 21:18:25 +0800

    Update dom.js

--- 通用方法库/dom.js
+++ 通用方法库/dom.js
@@ -1,5 +1,5 @@
 //获取class
-function getByClass(oParent, sClass){   
+let getByClass = function (oParent, sClass){   
     if(oParent.getElementsByClassName){
         return oParent.getElementsByClassName(sClass);
     }else{
@@ -15,32 +15,16 @@ function getByClass(oParent, sClass){
     }
 }
 
-//增加class
-function addClass(obj,cls) {
-    var obj_class=obj.className,//获取class的内容；
-    blank = ( obj_class != '' ) ? ' ' : '';//判断获取的class是否为空，如果不为空，则添加空格；
-    added = obj_class + blank + cls;//组合原来的class和需要添加的class，中间加上空格；
-    obj.className = added;//替换原来的class；
+let hasClass = function ( elements,cName ){
+    return !!elements.className.match( new RegExp( "(\\s|^)" + cName + "(\\s|$)") )
 }
-
-//移除class
-function removeClass(obj,cls){
-    var obj_class = '' + obj.className + '';//获取class的内容，并在首尾各加一个空格；'abc    bcd' -> ' abc    bcd '
-    obj_class = obj_class.replace(/(\s+)/gi,' ');//将多余的空字符替换成一个空格；' abc    bcd ' -> ' abc bcd '
-    removed = obj_class.replace(' '+cls+' ',' ');//在原来的class，替换掉首尾加了空格的class  ' abc bcd ' -> 'bcd '
-    removed = removed.replace(/(^\s+)|(\s+$)/g,'');//去掉首尾空格；'bcd ' -> 'bcd'
-    obj.className = removed;//替换原来的class；
+let addClass = function ( elements,cName ){
+    if( !hasClass( elements,cName ) ){
+        elements.className += " " + cName
+    };
 }
-
-//判断是否存在class
-function hasClass(obj,cls){
-    var obj_class = obj.className,//获取class的内容；
-    obj_class_lst = obj_class.split(/\s+/);//通过split空字符将cls转换成数组
-    x = 0;
-    for(x in obj_class_lst){
-        if ( obj_class_lst[x] == cls ) {
-            return true;
-        }
-    }
-    return false;
+let removeClass = function ( elements,cName ){
+    if( hasClass( elements,cName ) ){
+        elements.className = elements.className.replace( new RegExp( "(\\s|^)" + cName + "(\\s|$)" ), " " ).trim()
+    };
 }
